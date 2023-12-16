de8356e479c5b68d22d2167f139203ce67093719
Justine George <justinegeo96@gmail.com>
2023-12-16 13:34:53 -0600

    Remove unused imports

--- src/test/java/com/jgeorge/springsecurityjwtdemo/controller/AdminControllerTest.java
+++ src/test/java/com/jgeorge/springsecurityjwtdemo/controller/AdminControllerTest.java
@@ -2,7 +2,6 @@ package com.jgeorge.springsecurityjwtdemo.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.jgeorge.springsecurityjwtdemo.controller.AdminController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
